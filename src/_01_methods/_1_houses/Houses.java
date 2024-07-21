08f47fc05f2da76df511e7f2d591563418e2557a
league <league@imac-16.attlocal.net>
2024-07-21 11:29:36 -0700

    I do more next week it was my birthday

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,39 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
+	   public static void main(String[] args) {
+		   
+		Robot rob=new Robot();
+		rob.setX(0);
+		rob.setY(400);
+		   // Check the recipe to find out what code to put here
+		   for(int i=0;i<=10;i++) {
+		iDraw(rob);
+		   }
 		
-	public void run() {
-		// Check the recipe to find out what code to put here
 	}
+public static void iDraw(Robot rob) {
+	
+	rob.penDown();
+	rob.setSpeed(200);
+	rob.setPenWidth(25);
+	rob.move(100);
+	rob.turn(90);
+	rob.move(40);
+	rob.turn(90);
+	rob.move(100);
+	rob.turn(-90);
+	rob.setPenColor(0,255,0);
+	rob.move(30);
+	rob.turn(-90);
 }
+		
+	}
+
+	''
--- src/_01_methods/_1_houses/HousesRunner.java
+++ src/_01_methods/_1_houses/HousesRunner.java
@@ -8,9 +8,10 @@ public class HousesRunner {
 	public static void main(String[] args) {
 		//Here we call the run() method from the Houses class
 		//Do not add any more code here!
-		
+		public static void run() {
+
 		Houses houseBuilder = new Houses();
-		houseBuilder.run();
+		 houseBuilder.run();
 
 
 	}
