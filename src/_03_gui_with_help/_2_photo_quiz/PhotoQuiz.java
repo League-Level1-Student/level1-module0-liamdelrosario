d6793b7b766cf7042420ab38f406820b5f33282a
league <league@imac-39.attlocal.net>
2024-10-19 17:56:15 -0700

    I think i need this month to finish this module

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -33,20 +33,49 @@ public class PhotoQuiz {
 		// 4. add the image to the quiz window
 		quizWindow.add(image1);
 		quizWindow.pack();
+
 		// 5. call the pack() method on the quiz window
 
+
 		// 6. ask a question that relates to the image
+		String healG = JOptionPane.showInputDialog("Is this guinea pig a chess grandmaster and is he better than you in anything no matter whay your race or identity is yes or no.");
 
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(healG.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "You are super right he is better than you, you know despite you do not want to accept it people would always be better than no matter what the topic is about.");
+			System.out.println("correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
+		if(healG.equals("no")) {
+			JOptionPane.showMessageDialog(null, "Meet me at fourth street avanue this mourning we have some 'Arranged' work to do.");
+			System.out.println("incorrect");
+		}	else {
+			JOptionPane.showMessageDialog(null, "Youre wrong");
+		}
+
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+		
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		quizWindow.remove(image1);
+		image1=createImage("file:///Users/league/git/level1-module0-liamdelrosario/images/images.jpg");
+		quizWindow.add(image1);
+		quizWindow.pack();
+		String j = JOptionPane.showInputDialog("If you have parents that expected you to have only A++ what is your blood type");
+		if(j.equals("A+")) {
+			System.out.println("correct");
+			JOptionPane.showMessageDialog(null, "╔═╦╗╔╦╗╔═╦═╦╦╦╦╗╔═╗\n"
+											  + "║╚╣║║║╚╣╚╣╔╣╔╣║╚╣═╣ \n"
+											  + "╠╗║╚╝║║╠╗║╚╣║║║║║═╣\n"
+											  + "╚═╩══╩═╩═╩═╩╝╚╩═╩═╝");
+		}
+		else {
+			System.out.println("incorrect");
+			JOptionPane.showMessageDialog(null, "):<):<):<):<):<):<):):<):<):<");
+		}
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
@@ -68,7 +97,7 @@ public class PhotoQuiz {
 			// TODO Auto-generated catch block
 			JOptionPane.showMessageDialog(null, "I can't find your image!!");
 		}
-		
+
 		return imageLabel;
 	}
 
--- src/_03_gui_with_help/_3_drum_kit/DrumKit.java
+++ src/_03_gui_with_help/_3_drum_kit/DrumKit.java
@@ -6,116 +6,123 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import game_tools.Sound;
 
 public class DrumKit implements MouseListener {
-    static boolean canPlaySounds = true; // Set this to false if your computer cannot play sounds
-    JLabel drumLabel;
+	static boolean canPlaySounds = true; // Set this to false if your computer cannot play sounds
+	JLabel drumLabel;
 
-    public void run() {
-        //  Make a JFrame variable and initialize it using "new JFrame()"
+	public void run() {
+		//  Make a JFrame variable and initialize it using "new JFrame()"
+		JFrame frame = new JFrame();
 
-        //  Make the frame visible and
-        // set its default close operation to JFrame.EXIT_ON_CLOSE
+		//  Make the frame visible and
+		// set its default close operation to JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //  Set the title of the frame
+		//  Set the title of the frame
 
-        //  Make a JPanel variable and initialize it using "new JPanel().
+		//  Make a JPanel variable and initialize it using "new JPanel().
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
 
-        //  Add the panel to the frame. (The panel is invisible.)
+		frame.add(panel);
 
-        //  Use the image of a drum provided ("snare.jpg") or
-        //  download an image of a drum from the Internet and drop it into the
-        //  Drum Kit recipe package.
+		//  Add the panel to the frame. (The panel is invisible.)
 
-        //  Put the name of the drum image file in a String variable.
+		//  Use the image of a drum provided ("snare.jpg") or
+		//  download an image of a drum from the Internet and drop it into the
+		//  Drum Kit recipe package.
 
-        //  Edit the next line to use your String variable
-        // drumLabel = createLabelImage(drumImageString);
+		//  Put the name of the drum image file in a String variable.
+		drumLabel = createLabelImage("snare.jpg");
+		//  Edit the next line to use your String variable
+		// drumLabel = createLabelImage(drumImageString);
+		//  Add the label to the panel
+		frame.pack();
+		//  Call the pack() method on the frame.
+		// Run your program. Do you see your drum image?
 
-        //  Add the label to the panel
+		// Add this MouseListener to drumLabel
 
-        //  Call the pack() method on the frame.
-        // Run your program. Do you see your drum image?
+		// *** Write the code in the mouseClicked() method below
 
-        // Add this MouseListener to drumLabel
+		//  Set the layout of the panel to "new GridLayout()"
 
-        // *** Write the code in the mouseClicked() method below
+		//  Add a cymbal image to make a Drum Kit (one has been provided).
+		//  You will need a different sound to go with this image.
+		//  Remember to add this MouseListener to it. Run the program.
 
-        //  Set the layout of the panel to "new GridLayout()"
+	}
 
-        //  Add a cymbal image to make a Drum Kit (one has been provided).
-        //  You will need a different sound to go with this image.
-        //  Remember to add this MouseListener to it. Run the program.
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// Print "mouse clicked" to the console. Run your program and watch
+		// the console to see when this is printed.
 
-    }
+		//JLabel labelClicked = (JLabel) e.getSource(); // This line gets the label
+		// that the mouse
+		// clicked on
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // Print "mouse clicked" to the console. Run your program and watch
-        // the console to see when this is printed.
+		// You can use the drum sound provided ("drum.wav") or
+		// download another drum sound (.wav) and drop it into the Drum Kit package.
+		// You can find sounds on freesound.org, and to download from there, you must log in
+		// Ask your teacher for The League's login information.
 
-        //JLabel labelClicked = (JLabel) e.getSource(); // This line gets the label
-                                                        // that the mouse
-                                                        // clicked on
+		//  If the user clicks on the drumImage...use the playSound method to play the drum sound.
+		//  Test to see if it works before moving on.
 
-        // You can use the drum sound provided ("drum.wav") or
-        // download another drum sound (.wav) and drop it into the Drum Kit package.
-        // You can find sounds on freesound.org, and to download from there, you must log in
-        // Ask your teacher for The League's login information.
+	}
 
-        //  If the user clicks on the drumImage...use the playSound method to play the drum sound.
-        //  Test to see if it works before moving on.
-
-    }
-
-    private JLabel createLabelImage(String fileName) {
-        JLabel imageLabel = new JLabel();
-        URL imageURL = getClass().getResource(fileName);
-        if (imageURL == null) {
-            imageLabel.setText("Could not find image " + fileName);
-        } else {
-            Icon icon = new ImageIcon(imageURL);
-            imageLabel.setIcon(icon);
-        }
-        return imageLabel;
-    }
+	private JLabel createLabelImage(String fileName) {
+		JLabel imageLabel = new JLabel();
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			imageLabel.setText("Could not find image " + fileName);
+		} else {
+			Icon icon = new ImageIcon(imageURL);
+			imageLabel.setIcon(icon);
+		}
+		return imageLabel;
+	}
 
 
-    public static synchronized void playSound(String fileName) {
-        if (canPlaySounds) {
-            Sound s = new Sound("_03_gui_with_help/_3_drum_kit/" + fileName);
-            s.play();
-        } else {
-            JOptionPane.showMessageDialog(null, "Cannot play "+fileName, "No Sound", JOptionPane.ERROR_MESSAGE);
-        }
-    }
+	public static synchronized void playSound(String fileName) {
+		if (canPlaySounds) {
+			Sound s = new Sound("_03_gui_with_help/_3_drum_kit/" + fileName);
+			s.play();
+		} else {
+			JOptionPane.showMessageDialog(null, "Cannot play "+fileName, "No Sound", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
 }
